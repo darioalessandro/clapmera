05e8a852215b18ed5987c4bf1d51e1a899dfe611
Dario A Lencina-Talarico <darioalessandrolencina@gmail.com>
2020-09-04 21:19:04 -0400

    Upgrade structure to work on Mac OS 10.15 (#2)

--- Clapmera/Clapmera/CPNotifications.h
+++
@@ -1,9 +0,0 @@
-//
-//  CPNotifications.h
-//  Clapmera
-//
-//  Created by Dario Lencina on 6/16/13.
-//
-//
-
-static NSString * const AppDidBecomeActive= @"AppDidBecomeActive";
--- Clapmera/Pods/Target Support Files/FPPopover/FPPopover-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FPPopover : NSObject
-@end
-@implementation PodsDummy_FPPopover
-@end
